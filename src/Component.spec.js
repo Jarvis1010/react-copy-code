fc7c04f39c23e66ad8c841611da28c8476da85be
Travis Mair <travis.mair@gmail.com>
2018-01-16 11:52:45 -0700

    update publish to NPM process

--- src/Component.spec.js
+++
@@ -1,6 +0,0 @@
-import Component from './Component';
-import test from 'tape';
-
-test('<Component> shallow testing', t => {
-  t.end();
-});
--- src/ComponentStory.js
+++
@@ -1,6 +0,0 @@
-import { storiesOf, action } from '@kadira/storybook';
-import Component from './Component';
-
-storiesOf('Component', module).add('test one', () => (
-    <Component title="Hellooo" />
-));
