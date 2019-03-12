d3f82c53c7ff29b8669b3db25753df60c4d2204f
Jarvis1010 <travis.mair@gmail.com>
2019-03-12 12:20:06 -0600

    update to webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,26 @@
 var path = require("path");
+var nodeExternals = require("webpack-node-externals");
 module.exports = {
   entry: "./src/index.js",
+  mode: "production",
   output: {
-    path: path.resolve(__dirname, "build"),
+    path: path.resolve(__dirname, "dist"),
     filename: "index.js",
-    libraryTarget: "commonjs2", // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
+    library: "react-copy-code",
+    libraryTarget: "umd",
+    publicPath: "/dist/",
+    umdNamedDefine: true,
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         include: path.resolve(__dirname, "src"),
-        exclude: /(node_modules|bower_components|build)/,
+        exclude: /(node_modules|bower_components|dist)/,
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["env"],
+            presets: ["@babel/preset-env"],
             plugins: [
               "transform-object-rest-spread",
               "transform-class-properties",
@@ -26,7 +31,5 @@ module.exports = {
       },
     ],
   },
-  externals: {
-    react: "commonjs react", // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
-  },
+  externals: [nodeExternals()],
 };
